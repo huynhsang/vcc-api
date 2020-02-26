bcad83d90af7b02eed135fddc021a76c724907fb
sanght <sang.huynh@paradox.ai>
2020-02-26 09:42:03 +0700

    [Implement] Adding API /users/me

---
+++ common/models/user/_GetMyAccount.js
@@ -0,0 +1,23 @@
+/* global __ */
+import {notFoundErrorHandler} from '../../utils/modelHelpers';
+
+export default (User) => {
+    User._GetMyAccount = (req, callback) => {
+        if (!req.user) {
+            return callback(notFoundErrorHandler(__('err.user.notExists')));
+        }
+        // if (!req.user.isEnable) {
+        //     return callback(notFoundErrorHandler(__('err.user.notAvailable')));
+        // }
+        callback(null, req.user);
+    };
+
+    User.remoteMethod('_GetMyAccount', {
+        accepts: [
+            {arg: 'req', type: 'object', http: {source: 'req'}},
+        ],
+        description: 'Find my account',
+        returns: {type: 'object', model: 'user', root: true},
+        http: {path: '/me', verb: 'get'}
+    });
+};
