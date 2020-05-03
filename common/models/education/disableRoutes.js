14fb9b04f6b786582ed1e2799cfcfdd0a1a4ee63
huynhsang <thanhsangqn3@gmail.com>
2020-05-03 16:42:41 +0700

    [Update] Open PUT Education & Experiences APIs

--- common/models/education/disableRoutes.js
+++ common/models/education/disableRoutes.js
@@ -6,14 +6,14 @@ export default (Education) => {
     Education.disableRemoteMethodByName('update');
     // Education.disableRemoteMethodByName('find');
     Education.disableRemoteMethodByName('findById');
-    // Education.disableRemoteMethodByName('findOrCreate');
-    Education.disableRemoteMethodByName('replaceOrCreate');
+    Education.disableRemoteMethodByName('findOrCreate');
+    // Education.disableRemoteMethodByName('replaceOrCreate');
     Education.disableRemoteMethodByName('replaceById');
     Education.disableRemoteMethodByName('upsertWithWhere');
     Education.disableRemoteMethodByName('upsert');
     // Education.disableRemoteMethodByName('deleteById');
     Education.disableRemoteMethodByName('createChangeStream');
-    Education.disableRemoteMethodByName('prototype.updateAttributes');
+    // Education.disableRemoteMethodByName('prototype.updateAttributes');
     Education.disableRemoteMethodByName('prototype.replaceAttributes');
     Education.disableRemoteMethodByName('prototype.__get__owner');
 };
--- common/models/experience/disableRoutes.js
+++ common/models/experience/disableRoutes.js
@@ -6,14 +6,14 @@ export default (Experience) => {
     Experience.disableRemoteMethodByName('update');
     // Experience.disableRemoteMethodByName('find');
     Experience.disableRemoteMethodByName('findById');
-    // Experience.disableRemoteMethodByName('findOrCreate');
-    Experience.disableRemoteMethodByName('replaceOrCreate');
+    Experience.disableRemoteMethodByName('findOrCreate');
+    // Experience.disableRemoteMethodByName('replaceOrCreate');
     Experience.disableRemoteMethodByName('replaceById');
     Experience.disableRemoteMethodByName('upsertWithWhere');
     Experience.disableRemoteMethodByName('upsert');
     // Experience.disableRemoteMethodByName('deleteById');
     Experience.disableRemoteMethodByName('createChangeStream');
-    Experience.disableRemoteMethodByName('prototype.updateAttributes');
+    // Experience.disableRemoteMethodByName('prototype.updateAttributes');
     Experience.disableRemoteMethodByName('prototype.replaceAttributes');
     Experience.disableRemoteMethodByName('prototype.__get__owner');
 };
