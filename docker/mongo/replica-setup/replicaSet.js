60aca0576bc99f0ba09b1a27f357cb5521f388e0
huynhsang <thanhsangqn3@gmail.com>
2020-06-08 22:24:16 +0700

    [Update] Docker compose MongoDB

--- docker/mongo/replica-setup/replicaSet.js
+++ docker/mongo/replica-setup/replicaSet.js
@@ -2,10 +2,11 @@ rsconf = {
     _id: 'vcc-replica-set',
     members: [
         {_id: 0, host: 'vcc-mongo-primary:27017', priority: 2},
-        {_id: 1, host: 'vcc-mongo-worker-1:27017'},
-        {_id: 2, host: 'vcc-mongo-worker-2:27017'}
+        {_id: 1, host: 'vcc-mongo-worker-1:27017', priority: 0},
+        {_id: 2, host: 'vcc-mongo-worker-2:27017', priority: 0}
     ]
 };
 
 rs.initiate(rsconf);
 rs.conf();
+db.getMongo().setReadPref('nearest');
